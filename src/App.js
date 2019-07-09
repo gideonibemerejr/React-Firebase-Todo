dbfadb18fb5b6ac8e024e36f9ce4413d49d7e2e2
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-09 12:04:27 -0500

    User story 1 complete

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-imrc
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Welcome to React Firebase Todos</h1>
+         );
+    }
+}
+ 
+export default App;
